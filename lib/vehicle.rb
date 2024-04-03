834f3a7a7959ddaea4103c7a4f4bfa71c3e2aa2e
Chee Lee <cheeleertr@gmail.com>
2024-04-03 10:39:34 -0500

    Add vehicle class w/attributes

---
+++ lib/vehicle.rb
@@ -0,0 +1,14 @@
+class Vehicle
+  attr_reader :year,
+              :make,
+              :model,
+              :speeding
+  
+  def initialize(year, make, model)
+    @year = year
+    @make = make
+    @model = model
+    @speeding = false
+  end
+  
+end
--- spec/vehicle_spec.rb
+++ spec/vehicle_spec.rb
@@ -11,28 +11,27 @@ RSpec.describe do
       expect(vehicle.model).to eq("Civic")
     end
     
-    it "can check if it's speeding"
-      vehicle = Vehicle.new("2001", "Honda", "Civic")
+    # it "can check if it's speeding" do
+    #   vehicle = Vehicle.new("2001", "Honda", "Civic")
 
-      expect(vehicle.speeding?).to eq(false)
-      expect(vehicle.speed).to eq(false)
-      expect(vehicle.speed).to eq()
-      expect(vehicle.speeding?).to eq(true)
+    #   expect(vehicle.speeding?).to eq(false)
+    #   expect(vehicle.speed).to eq(false)
+    #   expect(vehicle.speed).to eq()
+    #   expect(vehicle.speeding?).to eq(true)
+    # end
 
-    end
+    # it "can add passengers" do
+    #   vehicle = Vehicle.new("2001", "Honda", "Civic")
 
-    it "can add passengers" do
-      vehicle = Vehicle.new("2001", "Honda", "Civic")
+    #   expect(vehicle.passengers).to eq([])
+    #   charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    #   jude = Passenger.new({"name" => "Jude", "age" => 20})
+    #   taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+    #   vehicle.add_passenger(charlie)
+    #   vehicle.add_passenger(jude)
+    #   vehicle.add_passenger(taylor)
 
-      expect(vehicle.passengers).to eq([])
-      charlie = Passenger.new({"name" => "Charlie", "age" => 18})
-      jude = Passenger.new({"name" => "Jude", "age" => 20})
-      taylor = Passenger.new({"name" => "Taylor", "age" => 12})
-      vehicle.add_passenger(charlie)
-      vehicle.add_passenger(jude)
-      vehicle.add_passenger(taylor)
-
-      expect(vehicle.passengers).to eq([charlie, jude, taylor])
-      expect(vehicle.num_adults).to eq(3)
-    end
+    #   expect(vehicle.passengers).to eq([charlie, jude, taylor])
+    #   expect(vehicle.num_adults).to eq(3)
+    # end
   end
